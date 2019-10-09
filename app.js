514e7455fcf0c722e5a6a45703eb78bfe2adce07
zhangshengpeng <1612977540@qq.com>
2019-10-09 23:37:14 +0800

    ..

--- app.js
+++ app.js
@@ -146,8 +146,7 @@ app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1');
-  res.header("Content-Type", "text/html;charset=utf-8");
+  res.header("X-Powered-By",' 3.2.1'); 
   next();
 });
 
