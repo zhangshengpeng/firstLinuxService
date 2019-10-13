c054001376371f372301f3195f9b8f761847d3a0
zhangshengpeng <1612977540@qq.com>
2019-10-13 13:04:21 +0800

    ..

--- app.js
+++ app.js
@@ -195,7 +195,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+  res.header("Access-Control-Allow-Origin", req.origin);
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
   res.header('Access-Control-Allow-Credentials', 'true');
