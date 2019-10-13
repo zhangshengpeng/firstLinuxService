d14d92d34f3b8dba8f6997f7c410c015f0ef15d9
zhangshengpeng <1612977540@qq.com>
2019-10-13 23:04:14 +0800

    ,,

--- route.js
+++ route.js
@@ -225,9 +225,10 @@ exports.Friend = function(req,res){
 	}
 
 exports.setGameResult = (req, res)=>{ 
-	let sql = "INSERT INTO gamelist(houseId, type, userA, userB, userAOperation, userBOperation, date) VALUE (?,?,?,?,?,?,?)"
-	let date = new Date()
-	let params = [req.houseId, req.type, req.userA, req.userB, req.userAOperation, rsq.userBOperation, date]
+	console.log(res)
+	let sql = "INSERT INTO gamelist(houseId, type, userA, userB, userAOperation, userBOperation) VALUE (?,?,?,?,?,?)"
+	// let date = new Date()
+	let params = [req.houseId, req.type, req.userA, req.userB, req.userAOperation, rsq.userBOperation]
 	connection.query(sql, params, (err, result)=>{
 		if (result) {
 			console.log("插入成功")
