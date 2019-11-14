1bde9bbf8972719d032a011e47a86ae9d0523449
zhangshengpeng <1612977540@qq.com>
2019-11-14 13:59:53 +0800

    重大更新，独立出socket逻辑模块

--- app.js
+++ app.js
@@ -6,17 +6,11 @@ var logger = require('morgan');
 var multer = require('multer')
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 //gzip插件
 let compression = require('compression')
 
 var Router = require('./route');
 
-
-
-
-var arrAllSocket = []
-let houseList = []
 
 //图片存储
 var storage = multer.diskStorage({
@@ -28,178 +22,6 @@ var storage = multer.diskStorage({
   }
 })
 var upload = multer({ storage: storage });
-
-io.on('connection',function(socket){
-  socket.on('online', function(id){
-    console.log(id,"加入聊天室")
-    arrAllSocket[id] = socket.id
-    console.log("online:",arrAllSocket)
-    io.emit('houseList', houseList)
-  })
-  //创建房间
-  socket.on('createHouse',(data)=>{
-    console.log('创建参数',data)
-    let params = {
-      houseId: '',
-      type: data.type,
-      user: []
-    }
-    if(houseList.length===0){
-      console.log('插入第一个房间')
-      houseList.push({
-        houseId: 0,
-        type: data.type,
-        user: [{
-          id: data.userId,
-          name: data.userName,
-          url: data.userUrl,
-          state: 0,
-          operation:0
-        }]
-      })
-    } else {
-      for(let i=0;i<houseList.length;i++) {
-        if(houseList[i].houseId!=i) {
-          params.houseId = i;
-          let user = {
-            id: data.userId,
-            name: data.userName,
-            url: data.userUrl,
-            state: 0,
-            operation:0
-          }
-          params.user.push(user)
-          houseList.splice(i, 0, params)
-          console.log('房间状态',houseList)
-          break;
-        }
-        if(i==(houseList.length-1)){
-          console.log('末尾添加房间')
-          houseList.push({
-            houseId: i+1,
-            type: data.type,
-            user: [{
-              id: data.userId,
-              name: data.userName,
-              url: data.userUrl,
-              state: 0,
-              operation:0
-            }]
-          })
-          break;
-        }
-      }
-    }
-    io.emit('houseList', houseList)
-    io.to(arrAllSocket[data.userId]).emit('createHouse',houseList);
-  })
-  //用户进入
-  socket.on('addUser', (data)=>{
-    console.log('请求插入：', data)
-    houseList.forEach((item, index)=>{
-      console.log(item)
-      if(item.houseId == data.houseId) {
-        let params = {
-          id: data.userId,
-          name: data.userName,
-          url: data.userUrl,
-          state: 0,
-          operation:0
-        }
-        houseList[index].user.push(params)
-        houseList[index].user.forEach((item)=>{
-          io.to(arrAllSocket[item.id]).emit('addUser',houseList[index]);
-        })
-        console.log('插入结果', houseList)
-      }
-    })
-    io.emit('houseList', houseList)
-  })
-  //用户离开
-  socket.on('userLeave', (data)=>{
-    houseList.forEach((item, index)=>{
-      if(item.houseId==data.houseId) {
-        item.user.forEach((u, number)=>{
-          if(u.id==data.userId){
-            houseList[index].user.splice(number, 1)
-            if(houseList[index].user.length===0) {
-              houseList.splice(index,1)
-            }
-          }
-        });
-      }
-    })
-    io.emit('houseList', houseList)
-  })
-  //准备
-  socket.on('ready', (data)=>{
-    console.log('状态修改', data)
-    houseList.forEach((item, index)=>{
-      if(item.houseId==data.houseId) {
-        item.user.forEach((u, number)=>{
-          if(u.id==data.userId) {
-            houseList[index].user[number].state = data.state
-            houseList[index].user[number].operation = 0
-            houseList[index].user.forEach((i)=>{
-              io.to(arrAllSocket[i.id]).emit('ready',houseList[index]);
-            })
-          }
-        })
-      }
-    })
-  })
-  //出拳
-  socket.on('done',(data)=>{
-    console.log('出拳', data)
-    houseList.forEach((item,index)=>{
-      if(item.houseId==data.houseId) {
-        item.user.forEach((u,number)=>{
-          if(u.id==data.userId) {
-            houseList[index].user[number].operation = data.operation
-            houseList[index].user.forEach((u)=>{
-              io.to(arrAllSocket[u.id]).emit('operation',houseList[index]);
-            })
-            console.log('出拳结果', houseList[index])
-          }
-        })
-      }
-    })
-  })
-  socket.on('houseList',()=>{
-    console.log('请求大厅列表')
-    io.emit('houseList', houseList)
-  })
-  //消息
-  socket.on('message',function(str){
-    console.log(str)
-    io.to(arrAllSocket[str.to]).emit('message',str);
-    io.to(arrAllSocket[str.say]).emit('message',str);
-    console.log("已发送")
-    Router.saveMsg(str)
-  })
-  socket.on('disconnect',function(){
-    console.log('用户离开', socket.id)
-    arrAllSocket.forEach((item, index)=>{
-      if(item==socket.id) {
-        console.log('匹配成功')
-        houseList.forEach((house, I)=>{
-          house.user.forEach((u,i)=>{
-            if(u.id==index) {
-              console.log('离开用户：', u)
-              house.user.splice(i,1)
-              if(house.user.length==0) {
-                houseList.splice(I, 1)
-              }
-            }
-          })
-        })
-        arrAllSocket.splice(index,1)
-        console.log("队列：",arrAllSocket)
-      }
-    })
-    io.emit('houseList', houseList)
-  })
-})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -253,4 +75,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-http.listen(8080)
+
